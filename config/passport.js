600ccbba81de798464f2f713caf717444fadbb40
Dhaya Raj <dhayarajrajendran00@gmail.com>
2025-03-16 21:53:19 +0530

    Updated Google OAuth callback URL logic

--- config/passport.js
+++ config/passport.js
@@ -4,17 +4,14 @@ const User = require("../models/userSchema");
 const env = require("dotenv").config();
 const crypto = require("crypto");
 
-const callbackURL =
-  process.env.NODE_ENV === "production"
-    ? "https://puzlmart.shop/auth/google/callback"
-    : "http://localhost:3000/auth/google/callback";
-
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: callbackURL,
+      callbackURL: process.env.NODE_ENV === "production"
+      ? "https://puzlmart.shop/auth/google/callback"
+      : "http://localhost:3000/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
