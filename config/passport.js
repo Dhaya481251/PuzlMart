ee87a1f724b2b741b2f9d0b756bd3648dd672937
Dhaya Raj <dhayarajrajendran00@gmail.com>
2025-03-16 22:21:02 +0530

    passport.js changed

--- config/passport.js
+++ config/passport.js
@@ -9,9 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV === "production"
-      ? "https://puzlmart.shop/auth/google/callback"
-      : "http://localhost:3000/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
