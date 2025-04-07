3699db3d7729bdb44a2586bf394f68fb23b0e5ac
Dhaya Raj <dhayarajrajendran00@gmail.com>
2025-04-07 23:49:20 +0530

    enumerate the status codes and add the server error modal in products page

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -10,7 +10,76 @@ const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
 
+const {StatusCodes,ResponsePhrases} = require('http-status-codes');
+
+const fetchDashboardData = async() => {
+  const users = await User.find({isAdmin:false});
+  const orders = await Order.find()
+  .sort({createdOn:-1})
+  .populate('items.productId')
+  .populate('userId');
+  const products = await Product.find();
+  const notifications = await Notification.find({notificationType:'returnRequest'})
+  .populate('orderId')
+  .sort({createdOn:-1});
+
+  const topSellingProducts = await Order.aggregate([
+    {$unwind:'$items'},
+    {$group:{_id:'$items.productId',totalQuantitySold:{$sum:'$items.quantity'}}},
+    {$sort:{totalQuantitySold:-1}},
+    {$limit:10},
+    {$lookup:{from:'products',localField:'_id',foreignField:'_id',as:'productDetails'}},
+    {$unwind:'$productDetails'}
+  ]);
+  const topSellingCategories = await Order.aggregate([
+    {$unwind:'$items'},
+    {$lookup:{from:'products',localField:'items.productId',foreignField:'_id',as:'productDetails'}},
+    {$unwind:'$productDetails'},
+    {$group:{_id:'$productDetails.category',totalQuantitySold:{$sum:'$items.quantity'}}},
+    {$sort:{totalQuantitySold:-1}},
+    {$limit:10},
+    {$lookup:{from:'categories',localField:'_id',foreignField:'_id',as:'categoryDetails'}},
+    {$unwind:'$categoryDetails'},
+    {$project:{_id:'$categoryDetails',totalQuantitySold:1}}
+  ])
+
+  const topSellingBrands = await Order.aggregate([
+    {$unwind:'$items'},
+    {$lookup:{from:'products',localField:'items.productId',foreignField:'_id',as:'productDetails'}},
+    {$unwind:'$productDetails'},
+    {$group:{_id:'$productDetails.brand',totalQuantitySold:{$sum:'$items.quantity'}}},
+    {$sort:{totalQuantitySold:-1}},
+    {$limit:10},
+    {$lookup:{from:'brands',localField:'_id',foreignField:'brandName',as:'brandDetails'}},
+    {$unwind:'$brandDetails'},
+    {$project:{brandName:'$_id',brandImage:'$brandDetails.brandImage',totalQuantitySold:1}}
+  ])
+  return {users,orders,products,notifications,topSellingProducts,topSellingCategories,topSellingBrands};
+};
+
+const fetchProductData = async() => {
+  const limit = 4;
+  const productData = await Product.find()
+  .populate('productOffer')
+  .populate('category');
+
+  const count = await Product.find().countDocuments();
+  const category = await Category.find({isListed:true});
+  const brand = await Brand.find({isBlocked:false});
+  const offer = await Offer.find({offerType:'Product'});
+  const notifications = await Notification.find({
+    notificationType:'returnRequest'
+  })
+  .populate('orderId')
+  .sort({createdOn:-1});
+
+  const totalPages = Math.ceil(count/limit);
+  
+  return {productData,totalPages,category,brand,offer,notifications}
+}
+
 const getProducts = async (req, res) => {
+  const {users,orders,products,notifications,topSellingProducts,topSellingCategories,topSellingBrands} = await fetchDashboardData();
   try {
     const search = req.query.search || "";
     const page = req.query.page;
@@ -47,22 +116,24 @@ const getProducts = async (req, res) => {
       res.render("product", {
         data: productData,
         currentPage: page,
-        totalPages: page,
         totalPages: Math.ceil(count / limit),
         cat: category,
         brand: brand,
         offer: offer,
         notifications,
+        errorMessage:null
       });
     } else {
-      res.status(404).send("Page not found");
+      res.status(StatusCodes.NOT_FOUND).render('product',{errorMessage:'Something went wrong! Please try again.'});
     }
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('admin-dashboard',{errorMessage:'Something went wrong! Please try again.',users,orders,products,notifications,topSellingProducts,topSellingCategories,topSellingBrands});
   }
 };
 
 const getProductAddPage = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
     const category = await Category.find({ isListed: true });
     const brand = await Brand.find({ isBlocked: false });
@@ -77,9 +148,10 @@ const getProductAddPage = async (req, res) => {
       brand: brand,
       product: products,
       notifications,
+      errorMessage:null
     });
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
@@ -111,13 +183,13 @@ const addProducts = async (req, res) => {
       const category = await Category.findOne({ name: products.category });
       if (!category) {
         return res
-          .status(400)
+          .status(StatusCodes.BAD_REQUEST)
           .json({ message: "Invalid category name", type: "error" });
       }
       if (images.length > 4) {
         return res.status(400).json({
           message: "No. of product should be less than or equal to 4",
-          type: "error",
+          type: "error"
         });
       }
       const newProduct = new Product({
@@ -130,32 +202,33 @@ const addProducts = async (req, res) => {
         createdOn: new Date(),
         quantity: products.quantity,
         productImage: images,
-
         relatedProducts: products.relatedProducts || [],
       });
 
       await newProduct.save();
 
       return res
-        .status(200)
+        .status(StatusCodes.OK)
         .json({ message: "Product added successfully", type: "success" });
     } else {
-      return res.status(400).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
         message: "Product already exist. Please try with another name.",
         type: "error",
       });
     }
   } catch (error) {
-    return res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:'Something went wrong! Please try again.',type:"error"});
   }
 };
 
 const loadAddProductOffer = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    const id = req.query.id;
-    const product = await Product.findOne({ _id: id }).populate("productOffer");
+    const productId = req.query.id;
+    const product = await Product.findOne({ _id: productId }).populate("productOffer");
     if (product.isBlocked === true) {
-      return res.status(400).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
         type: "error",
         message:
           "Product is blocked, so add product offer to this product is not possible",
@@ -166,9 +239,9 @@ const loadAddProductOffer = async (req, res) => {
     })
       .populate("orderId")
       .sort({ createdOn: -1 });
-    res.render("addProductOffer", { product, notifications });
+    res.render("addProductOffer", { product, notifications,errorMessage:null });
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
@@ -184,13 +257,13 @@ const addProductOffer = async (req, res) => {
 
     if (!findProduct) {
       return res
-        .status(404)
+        .status(StatusCodes.NOT_FOUND)
         .json({ type: "error", message: "Product not found" });
     }
     if (discountType === "Percentage") {
       if (value < 1 || value > 90) {
         return res
-          .status(400)
+          .status(StatusCodes.BAD_REQUEST)
           .json({ type: "error", message: "value must be between 1 and 90" });
       } else {
         const newOffer = new Offer({
@@ -209,12 +282,12 @@ const addProductOffer = async (req, res) => {
         await findProduct.save();
 
         res
-          .status(200)
+          .status(StatusCodes.OK)
           .json({ type: "success", message: "Offer added successfully" });
       }
     } else if (discountType === "Flat") {
       if (value >= findProduct.regularPrice) {
-        return res.status(400).json({
+        return res.status(StatusCodes.BAD_REQUEST).json({
           type: "error",
           message: "value must be less than product price",
         });
@@ -235,12 +308,12 @@ const addProductOffer = async (req, res) => {
         await findProduct.save();
 
         res
-          .status(200)
+          .status(StatusCodes.OK)
           .json({ type: "success", message: "Offer added successfully" });
       }
     }
   } catch (error) {
-    res.status(500).json({ type: "error", message: "Internal Server Error" });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ type: "error", message: "Something went wrong! Please try again." });
   }
 };
 
@@ -249,14 +322,14 @@ const removeProductOffer = async (req, res) => {
     const { productId } = req.body;
 
     if (!productId) {
-      return res.status(400).json({ message: "Product ID is required." });
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: "Product ID is required." });
     }
 
     const findProduct = await Product.findById(productId).populate(
       "productOffer"
     );
     if (!findProduct) {
-      return res.status(404).json({ message: "Product not found." });
+      return res.status(StatusCodes.NOT_FOUND).json({ message: "Product not found." });
     }
 
     if (findProduct.productOffer) {
@@ -283,45 +356,51 @@ const removeProductOffer = async (req, res) => {
 
       const removedOffer = await Offer.findByIdAndDelete(offerId);
 
-      return res.status(200).json({
+      return res.status(StatusCodes.OK).json({
         status: true,
         message: "Offer removed successfully.",
         removedOffer,
       });
     } else {
       return res
-        .status(404)
+        .status(StatusCodes.NOT_FOUND)
         .json({ message: "Offer not found for the product." });
     }
   } catch (error) {
-    return res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ type: "error", message: "Something went wrong! Please try again." });
   }
 };
 
 const blockProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    let id = req.query.id;
-    await Product.updateOne({ _id: id }, { $set: { isBlocked: true } });
+    let productId = req.query.id;
+    await Product.updateOne({ _id: productId }, { $set: { isBlocked: true } });
     res.redirect("/admin/products");
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
 const unblockProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    let id = req.query.id;
-    await Product.updateOne({ _id: id }, { $set: { isBlocked: false } });
+    let productId = req.query.id;
+    await Product.updateOne({ _id: productId }, { $set: { isBlocked: false } });
     res.redirect("/admin/products");
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
 const getEditProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    const id = req.query.id;
-    const product = await Product.findOne({ _id: id });
+    const productId = req.query.id;
+    const product = await Product.findOne({ _id: productId });
     const category = await Category.find({});
     const brand = await Brand.find({});
     const products = await Product.find({});
@@ -336,15 +415,18 @@ const getEditProduct = async (req, res) => {
       brand: brand,
       products: products,
       notifications,
+      errorMessage:null
     });
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
 const editProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    const id = req.params.id;
+    const productId = req.params.id;
     const data = req.body;
 
     const relatedProducts = Array.isArray(data.relatedProducts)
@@ -353,19 +435,19 @@ const editProduct = async (req, res) => {
       ? [data.relatedProducts]
       : [];
 
-    const product = await Product.findOne({ _id: id });
+    const product = await Product.findOne({ _id: productId });
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Product not found" });
     }
 
     const category = await Category.findOne({ name: data.category });
     if (!category) {
-      return res.status(400).json({ error: "Invalid category name" });
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid category name" });
     }
 
     const existingProduct = await Product.findOne({
       productName: data.productName,
-      _id: { $ne: id },
+      _id: { $ne: productId },
     });
     if (existingProduct) {
       return res.status(400).json({
@@ -410,10 +492,10 @@ const editProduct = async (req, res) => {
       updateFields.productImage = [...product.productImage, ...images];
     }
 
-    await Product.findByIdAndUpdate(id, updateFields, { new: true });
-    res.redirect(302, "/admin/products");
+    await Product.findByIdAndUpdate(productId, updateFields, { new: true });
+    res.redirect(StatusCodes.MOVED_TEMPORARILY, "/admin/products");
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
@@ -423,7 +505,7 @@ const deleteImage = async (req, res) => {
     const product = await Product.findById(productIdToServer);
 
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ error: "Product not found" });
     }
 
     product.productImage = product.productImage.filter(
@@ -431,51 +513,55 @@ const deleteImage = async (req, res) => {
     );
 
     await product.save();
-    res.json({ status: true });
+    res.status(StatusCodes.OK).json({ status: true });
   } catch (error) {
-    res.status(500).json({ status: false });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false });
   }
 };
 
 const removeProduct = async (req, res) => {
   try {
-    const id = req.query.id;
+    const productId = req.query.id;
 
-    if (!id) {
-      return res.status(400).send("Product ID is not provided");
+    if (!productId) {
+      return res.status(StatusCodes.BAD_REQUEST).json({status:false,message:"Product ID is not provided"});
     }
-    const result = await Product.deleteOne({ _id: id });
+    const result = await Product.deleteOne({ _id: productId });
 
     if (result.deletedCount === 0) {
-      return res.status(404).send("Product not found");
+      return res.status(StatusCodes.NOT_FOUND).json({status:false,message:"Product not found"});
     }
 
-    return res.status(200).send("Product deleted successfully");
+    return res.status(StatusCodes.OK).json({status:true,message:"Product deleted successfully"});
   } catch (error) {
     return res
-      .status(500)
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ status: false, message: "Internal Server Error" });
   }
 };
 
 const featuredProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    let id = req.query.id;
-    await Product.updateOne({ _id: id }, { $set: { featured: true } });
+    let productId = req.query.id;
+    await Product.updateOne({ _id: productId }, { $set: { featured: true } });
     res.redirect("/admin/products");
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
 const unFeaturedProduct = async (req, res) => {
+  const {productData,totalPages,category,brand,offer,notifications} = await fetchProductData();
+  const page = parseInt(req.query.page) || 1;
   try {
-    let id = req.query.id;
+    let productId = req.query.id;
 
-    await Product.updateOne({ _id: id }, { $set: { featured: false } });
+    await Product.updateOne({ _id: productId }, { $set: { featured: false } });
     res.redirect("/admin/products");
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('product',{errorMessage:'Something went wrong! Please try again.',data:productData,currentPage:page,totalPages,cat:category,brand,offer,notifications});
   }
 };
 
