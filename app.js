1bc7027a9975631481d1201d690daef9b6f6cd8f
Dhaya Raj <dhayarajrajendran00@gmail.com>
2025-04-13 15:13:55 +0530

    new changes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/", userRouter);
 app.use("/admin", adminRouter);
 
 app.use((req, res) => {
-  res.status(404).render('404Page')
+  res.status(404).redirect('/404');
 });
 
 app.get('/404', (req, res) => {
